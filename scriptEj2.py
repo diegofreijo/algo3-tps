ab3a97581727a3c3cf74f4651841e2608641bdb0
matiasblanco18 <matiasblanco18@0e191968-352c-0410-9b3c-71e71286eceb>
2007-03-28 03:03:04 +0000

    Sigo trabajando en este, falta terminarlo, pero poquito, banca massi :p

--- scriptEj2.py
+++ scriptEj2.py
@@ -1,23 +1,44 @@
 import random
 import sys
+import math
+def pertenece (a,b):
+	ret = False
+	i = 0
+	if a.count(b):
+		ret = True
+	return ret
+
 familia = sys.argv[1]
 familia = int(familia)
 enemigos = sys.argv[2]
 enemigos = int(enemigos)
-f=open("salida", 'w')
-f.write(str(familia))
-f.write(" ")
-f.write(str(enemigos))
-f.write('\n')
-i=0
-for i in range(0, enemigos):
-	enemigo1 = random.randrange(familia)
-	enemigo2 = random.randrange(familia)
-	while enemigo1 == enemigo2:
-		enemigo2 = random.randrange(familia)
-	f.write(str(enemigo1))
+instancias = sys.argv[3]
+instancias = int(instancias)
+x = pow(2,familia)
+if enemigos > x:
+	print "Los enemigos son mayores a la cantidad posible"
+else:
+	f=open("salida", 'w')
+	f.write(str(familia))
 	f.write(" ")
-	f.write(str(enemigo2))
+	f.write(str(enemigos))
 	f.write('\n')
-	i=i+1
-f.write(str(0))
+	for i in range (0, instancias):
+		a = []
+		i=0
+		while len(a) < enemigos:
+			print len(a)
+			enemigo1 = random.randrange(familia)
+			enemigo2 = random.randrange(familia)
+			while enemigo1 == enemigo2:
+				enemigo2 = random.randrange(familia)
+			b= enemigo1, enemigo2
+			if pertenece(a,b):
+				a = a
+			else:
+				a.append(b)
+			#f.write(str(enemigo1))
+			#f.write(" ")
+			#f.write(str(enemigo2))
+			#f.write('\n')
+	f.write(str(0))
