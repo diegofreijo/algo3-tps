40deb4f18ae4df64189cbb33b8f1090c220b1b1b
giga.freijo <giga.freijo@0e191968-352c-0410-9b3c-71e71286eceb>
2007-04-01 06:48:50 +0000

    Ahora si!, subo el Ej4 con la nueva remodelacion.

---
+++ trunk/ej4/Ej4_Pruebas.java
@@ -0,0 +1,61 @@
+package ej4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Ej4_Pruebas
+{
+	public static void Probar()
+	{
+		System.out.println("Probando Ej4:");
+		System.out.println("=============");
+		
+		List<Integer> factorizacion = new ArrayList<Integer>();
+		Integer productoria;
+		int errores = 0;
+		
+		for(int n = 2; n <= 1000000; ++n)
+		{
+			factorizacion = Ej4.Factorizacion(n);
+			System.out.print(n + ":  " + factorizacion.toString());
+			productoria = Probar_Productoria(factorizacion);
+			if(n != productoria) 
+			{
+				++errores;
+				System.out.println("	MAL!!!!! [" + errores + "]");
+			}
+			else
+			{
+				System.out.println("	BIEN!");
+			}
+		}
+		System.out.println("Fin de las pruebas, errores: " + errores);
+	}
+	
+	private static Integer Probar_Productoria(List<Integer> factorizacion)
+	{
+		Integer ret = 1;
+		for(Integer i : factorizacion)
+		{
+			ret *= i;
+		}
+		return ret;
+	}
+
+	public static void ProbarPrimos()
+	{
+		System.out.println("Probando primos:");
+		System.out.println("================");
+		
+		List<Integer> primos = new ArrayList<Integer>();
+		
+		for(int i = 2; i <= 500; ++i)
+		{
+			if(Ej4.EsPrimo(i,primos))
+			{
+				primos.add(i);
+				System.out.println(i);
+			}
+		}
+	}
+}
---
+++ trunk/ej4/Main.java
@@ -0,0 +1,9 @@
+package ej4;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Ej4.Ejecutar();
+	}
+}
